bf3d9f6bcc62076d16f59fb84d05551bd91a3da8
Andreas <andreaslund89@gmail.com>
2015-05-01 20:37:03 +0200

    Fjernet noe stuff.

--- index.js
+++ index.js
@@ -18,13 +18,3 @@ app.listen(app.get('port'), function() {
 });
 
 
-
-var pg = require('pg');
-
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-    var query = client.query('SELECT * FROM your_table');
-
-    query.on('row', function(row) {
-        console.log(JSON.stringify(row));
-    });
-});
