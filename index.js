03d132fbaecc2d4ffb9d0eabd02d1adfa208dac4
Andreas <andreaslund89@gmail.com>
2015-05-01 22:01:34 +0200

    utvider queries

--- index.js
+++ index.js
@@ -7,24 +7,36 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
+
+    var query = require('url').parse(request.url,true).query;
+    var user = query.user;
+
+    pg.connect("postgres://ebzqldzdjabrhx:7YBhRVZ3KanjSCuPvGtTYAIcBT@ec2-54-163-238-96.compute-1.amazonaws.com:5432/d2u36iutqq574u", function(err, client) {
+
+        var query = client.query("SELECT ANTALL FROM REQUESTER WHERE BRUKER = '" + user + "'");
+
+        query.on('row', function(row) {
+            response.send(JSON.stringify(row));
+        });
+    });
     response.send('GET request!');
 });
 
 app.post('/', function(request, response) {
+
+    pg.connect("postgres://ebzqldzdjabrhx:7YBhRVZ3KanjSCuPvGtTYAIcBT@ec2-54-163-238-96.compute-1.amazonaws.com:5432/d2u36iutqq574u", function(err, client) {
+
+        var query = client.query("INSERT INTO REQUESTER (ANTALL, BRUKER) VALUES ('2', 'testerbruker');");
+
+        query.on('row', function(row) {
+            response.send(JSON.stringify(row));
+        });
+    });
     response.send('POST request!');
-    
-    
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-    pg.connect("postgres://ebzqldzdjabrhx:7YBhRVZ3KanjSCuPvGtTYAIcBT@ec2-54-163-238-96.compute-1.amazonaws.com:5432/d2u36iutqq574u", function(err, client) {
 
-        var query = client.query("INSERT INTO REQUESTER (ANTALL, BRUKER) VALUES ('1', 'testbruker');");
-
-        query.on('row', function(row) {
-            console.log(JSON.stringify(row));
-        });
-    });
 });
 
