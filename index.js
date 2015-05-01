9af9711b8ee228bda4a85f701d2edf7ce5320930
Andreas <andreaslund89@gmail.com>
2015-05-01 20:27:46 +0200

    Skiller på GET og POST requester.

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    if (request.method === "GET") {
+        response.send('GET request!');
+    }
+    if (request.method === "POST"){
+        response.send('POST request!');
+    }
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+
+
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+    var query = client.query('SELECT * FROM your_table');
+
+    query.on('row', function(row) {
+        console.log(JSON.stringify(row));
+    });
 });
