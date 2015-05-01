ab167a8d594e0adb619bbaf67c554bc94fc45d6f
Andreas <andreaslund89@gmail.com>
2015-05-01 22:52:30 +0200

    test

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 
+var conString = "postgres://ebzqldzdjabrhx:7YBhRVZ3KanjSCuPvGtTYAIcBT@ec2-54-163-238-96.compute-1.amazonaws.com:5432/d2u36iutqq574u";
+var client = new pg.Client(conString);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -10,18 +12,22 @@ app.get('/', function(request, response) {
 
     var query = require('url').parse(request.url,true).query;
     var user = query.user;
-    
-    var result = function(){
-        return pg.connect("postgres://ebzqldzdjabrhx:7YBhRVZ3KanjSCuPvGtTYAIcBT@ec2-54-163-238-96.compute-1.amazonaws.com:5432/d2u36iutqq574u", function(err, client) {
 
-            var query = client.query("SELECT ANTALL FROM REQUESTER WHERE BRUKER = '" + user + "'");
-
-            query.on('row', function(row) {
-                return JSON.stringify(row);
-            });
+    client.connect(function(err) {
+        if(err) {
+            response.send('could not connect to postgres', err);
+        }
+        client.query("SELECT ANTALL FROM REQUESTER WHERE BRUKER = '" + user + "'", function(err, result) {
+            if(err) {
+                response.send('error running query', err);
+            }
+            console.log(result.rows[0]);
+            //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+            client.end();
+            response.send(result.rows[0].theTime);
         });
-    };
-
+    });
+    
 
     response.send(result);
 });
@@ -29,7 +35,7 @@ app.get('/', function(request, response) {
 app.post('/', function(request, response) {
 
     var result = "result";
-    pg.connect("postgres://ebzqldzdjabrhx:7YBhRVZ3KanjSCuPvGtTYAIcBT@ec2-54-163-238-96.compute-1.amazonaws.com:5432/d2u36iutqq574u", function(err, client) {
+    pg.connect(conString, function(err, client) {
 
         var query = client.query("INSERT INTO REQUESTER (ANTALL, BRUKER) VALUES ('2', 'testerbruker');");
 
