7d356d0619ce8a081e0446a374dfe8a71f819b6e
Andreas <andreaslund89@gmail.com>
2015-05-02 22:22:09 +0200

    test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var pg = require('pg');
 
 var conString = "postgres://ebzqldzdjabrhx:7YBhRVZ3KanjSCuPvGtTYAIcBT@ec2-54-163-238-96.compute-1.amazonaws.com:5432/d2u36iutqq574u";
+
 var client = new pg.Client(conString);
 client.connect();
 
@@ -11,14 +12,14 @@ var select_antall = function(request, response) {
     var urlquery = require('url').parse(request.url,true).query;
     var user = urlquery.user;
     
-    var query = client.query("SELECT ANTALL FROM REQUESTER WHERE BRUKER = '" + user + "'");
+    var query = client.query(selectUser(user));
 
 
     query.on("row", function (row, result) {
         result.addRow(row);
     });
     query.on("end", function (result) {
-// On end JSONify and write the results to console and to HTML output
+
         console.log(JSON.stringify(result.rows, null, "    "));
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write(JSON.stringify(result.rows) + "\n");
@@ -39,17 +40,45 @@ app.post('/', function(request, response) {
     var result = "result";
     pg.connect(conString, function(err, client) {
 
-        var query = client.query("INSERT INTO REQUESTER (ANTALL, BRUKER) VALUES ('2', 'testerbruker');");
+        var urlquery = require('url').parse(request.url,true).query;
+        var user = urlquery.user;
+        var antallQuery = client.query(selectUser(user));
 
-        query.on('row', function(row) {
-            result = JSON.stringify(row);
+        antallQuery.on("row", function (row, result) {
+            result.addRow(row);
+        });
+        antallQuery.on("end", function (result) {
+            var antall = result.rows;
+            if (antall === 0) {
+                var insertQuery = client.query(insertNew(user));
+                response.writeHead(200, {'Content-Type': 'text/plain'});
+                response.write(user + " er opprettet.\n");
+                response.end();
+            } else {
+                var updateQuery = client.query(updateAntall(user, antall));
+                response.writeHead(200, {'Content-Type': 'text/plain'});
+                response.write(user + " har fått antall justert til " + (++antall) + "\n");
+                response.end();
+            }
+           
         });
     });
-    response.send(result);
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 
 });
+
+function insertNew(user) {
+    return "INSERT INTO REQUESTER (ANTALL, BRUKER) VALUES ('1', '" + user + "');";
+}
+
+function updateAntall(user, antall) {
+    return "UPDATE REQUESTER SET ANTALL = '" + (++antall) + "' WHERE BRUKER = '" + user + "';";
+}
+
+function selectUser(user) {
+    return "SELECT ANTALL FROM REQUESTER WHERE BRUKER = '" + user + "';"
+}
 
