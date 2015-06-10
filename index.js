c6c93edb766db9d5f6a292953404bc8192d94f6b
Andreas <andreaslund89@gmail.com>
2015-06-10 21:50:42 +0200

    prøver å kommentere ut.

--- index.js
+++ index.js
@@ -65,9 +65,9 @@ app.post('/', function(request, response) {
             }
         });
         }
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write(svar);
-        response.end();
+     //   response.writeHead(200, {'Content-Type': 'text/plain'});
+     //   response.write(svar);
+     //   response.end();
     });
    
 });
