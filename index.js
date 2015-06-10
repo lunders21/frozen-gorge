a56d28cd3c8f3c7a852a1028c4f501162cc1656f
Andreas <andreaslund89@gmail.com>
2015-06-10 21:15:42 +0200

    Bommet litt

--- index.js
+++ index.js
@@ -79,6 +79,11 @@ app.get('/', function(request, response) {
             }
         });
     });
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write("ukjent request");
+    response.end();
+    return;
+});
 
 app.post('/', function(request, response) {
     pg.connect(conString, function(err, client) {
